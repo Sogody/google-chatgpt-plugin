f2e09f6035331bcb25f4f56c68ab13d9b543597c
Jamie Beck <jbeck@terabit.ca>
2023-04-30 02:28:04 -0400

    increase the number of results checked to 10

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def search():
         data = response.json()
         results = data.get('items', [])
         formatted_results = process_results(results, query)
-        return jsonify({"results": formatted_results[:5]})
+        return jsonify({"results": formatted_results[:10]})
     else:
         return jsonify({"error": "Error fetching search results"}), response.status_code
 
